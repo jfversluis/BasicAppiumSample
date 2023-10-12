b0d7205892afe25e7e620068301c8bcddc8da3cd
Gerald Versluis <gerald.versluis@microsoft.com>
2023-10-12 13:51:40 +0200

    Add UITests

---
+++ UITests/SetUpClass.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Appium.Service;
+
+namespace UITests;
+
+[SetUpFixture]
+public class SetUpClass
+{
+    AppiumLocalService? appiumLocalService;
+
+    [OneTimeSetUp]
+    public void RunBeforeAnyTests()
+    {
+        AppiumServiceBuilder builder = new();
+        builder.WithIPAddress("127.0.0.1");
+        builder.UsingPort(4723);
+
+        //Start the server with the builder
+        appiumLocalService = builder.Build();
+        appiumLocalService.Start();
+    }
+
+    [OneTimeTearDown]
+    public void RunAfterAnyTests()
+    {
+        appiumLocalService?.Dispose();
+    }
+}
+
